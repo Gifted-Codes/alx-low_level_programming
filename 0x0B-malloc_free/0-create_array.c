6fc7cf69521a52d0c2f1124a5ee933e6c0ce1bfd
Giftedgift98 <giftnduagah@gmail.com>
2023-10-04 15:04:14 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Main Entry
+ * @size: input
+ * @c: input
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+		char *arr;
+		unsigned int i;
+
+		if (size == 0)
+		{
+			return (NULL);
+		}
